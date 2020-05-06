a8f06853b8d92d4824e3d1c5761efd6615c38098
coolabhays <abhaysp9955@gmail.com>
2020-05-06 08:28:52 +0530

    templates which I used sometimes ago

---
+++ templates/incandmain.c
@@ -0,0 +1,18 @@
+/** Name -> Abhay Shanker Pathak
+Date -> <++>
+Description -> <++>
+Email -> abhaysp9955@gmail.com
+Github -> https://github.com/CoolAbhayS
+Last Edit -> <++> */
+
+#include<stdio.h>
+<++>
+
+// main function <++>
+int main(int argc, char *argv[]){
+	<++>
+	return 0;
+}
+// >>>
+
+<++>
