2ab52e0d347f222263b3999552e0b781ed02c2c3
coolabhays <abhaysp9955@gmail.com>
2021-03-07 08:09:18 +0530

    more treesitter config

--- lua/treesitter-config.lua
+++ lua/treesitter-config.lua
@@ -1,7 +1,24 @@
-require'nvim-treesitter.configs'.setup {
+require'nvim-treesitter.configs'.setup({
 	ensure_installed = "maintained", -- one of "all", "maintained"(parsers with maintainers) or list of languages
 	highlight = {
 		enable = true,  -- false will disable the whole extension
 		disable = {},
 	},
-}
+	incremental_selection = {
+		enable = true,
+		keymaps = {
+			init_selection = "gnn",
+			node_incremental = "gnn",
+			scope_incremental = "gnc",
+			node_decremental = "gnd",
+		},
+	},
+	indent = {
+		enable = true
+	},
+})
+
+vim.api.nvim_exec([[
+	set foldmethod=expr
+	set foldexpr=nvim_treesitter#foldexpr()
+	]], true)
